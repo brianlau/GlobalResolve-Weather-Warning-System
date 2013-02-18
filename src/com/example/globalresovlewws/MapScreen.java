025f4f92f8b7ed2bb912945ae3537e8f92b84722
Scott Camarena <scamaren@asu.edu>
2013-02-18 14:10:41 -0700

    Manifest updated?

--- src/com/example/globalresovlewws/MapScreen.java
+++ src/com/example/globalresovlewws/MapScreen.java
@@ -1,9 +1,10 @@
 package com.example.globalresovlewws;
 
-import android.app.Activity;
+//import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-public class MapScreen extends Activity {
+public class MapScreen extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
