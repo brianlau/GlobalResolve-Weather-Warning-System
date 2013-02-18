482345e434ddbd3daaa2781ab34a0a6e7ad64df9
Brian Lau <ir00ber@gmail.com>
2013-02-18 13:06:38 -0700

    Finished up dbhandler tests, commented out siren

--- src/com/example/globalresovlewws/MainActivity.java
+++ src/com/example/globalresovlewws/MainActivity.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.res.AssetFileDescriptor;
 import android.view.Menu;
@@ -16,20 +15,22 @@ import android.widget.TextView;
 import android.os.Handler;
 
 public class MainActivity extends Activity {
-	MediaPlayer mp = new MediaPlayer();
-	AssetFileDescriptor siren;
-	boolean flag = false;
+//	MediaPlayer mp = new MediaPlayer();
+//	AssetFileDescriptor siren;
+//	boolean flag = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//Opens a database on the Android device
 		DatabaseHandler db = new DatabaseHandler(this);
 	
 		final SimulateData sim = new SimulateData();
 		final int[] imageArray = { R.drawable.sunny_icon, R.drawable.night_rain };
 
-		siren = getResources().openRawResourceFd(R.raw.siren);
+//		siren = getResources().openRawResourceFd(R.raw.siren);
 
 		final TextView textView = (TextView) findViewById(R.id.temp_curr);
 		final ImageView imageView = (ImageView) findViewById(R.id.imageView);
@@ -40,30 +41,30 @@ public class MainActivity extends Activity {
 
 			public void run() {
 				sim.TemperatureForecast(); // run dummyTemp
-				try {
-					mp.setDataSource(siren.getFileDescriptor(),
-							siren.getStartOffset(), siren.getLength());
-					mp.prepare();
-				} catch (IllegalArgumentException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IllegalStateException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+//				try {
+//					mp.setDataSource(siren.getFileDescriptor(),
+//							siren.getStartOffset(), siren.getLength());
+//					mp.prepare();
+//				} catch (IllegalArgumentException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				} catch (IllegalStateException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				} catch (IOException e) {
+//					// TODO Auto-generated catch block
+//					e.printStackTrace();
+//				}
 				if (sim.CurrentTemperature() <= 30) { // sets to rain
 					i = 1;
-					mp.start();
+//					mp.start();
 
 				} else if (sim.CurrentTemperature() > 30) { // sets to sun
 					i = 0;
-					if (mp != null && mp.isPlaying()) {
-						mp.stop();
-						mp.reset();
-					}
+//					if (mp != null && mp.isPlaying()) {
+//						mp.stop();
+//						mp.reset();
+//					}
 				}
 				imageView.setImageResource(imageArray[i]); // sets image
 				textView.setText(Integer.toString(sim.CurrentTemperature())
@@ -103,22 +104,22 @@ public class MainActivity extends Activity {
 	@Override 
 	public void onPause(){
 	    super.onPause();
-	    if(mp != null){
-	    	mp.release();
-	    }
+//	    if(mp != null){
+//	    	mp.release();
+//	    }
 
 	}
 
 	@Override
 	public void onResume(){
 		super.onResume();
-		if(mp == null)
-			initializeMediaPlayer();
+//		if(mp == null)
+//			initializeMediaPlayer();
 	}
-	private void initializeMediaPlayer() {
-		mp = new MediaPlayer();
-		
-	}
+//	private void initializeMediaPlayer() {
+//		mp = new MediaPlayer();
+//		
+//	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
--- src/com/example/globalresovlewws/Tests/DatabaseHandlerTest.java
+++ src/com/example/globalresovlewws/Tests/DatabaseHandlerTest.java
@@ -4,20 +4,19 @@ import java.util.List;
 
 import android.content.Context;
 import android.test.AndroidTestCase;
-import android.util.Log;
-
 import com.example.globalresovlewws.DatabaseHandler;
 import com.example.globalresovlewws.Weather;
 
 import org.junit.Test;
 
-public class DatabaseHandlerTest extends AndroidTestCase{
+public class DatabaseHandlerTest extends AndroidTestCase {
 
 	private DatabaseHandler db;
 	private Weather weather1;
 	private Context context;
+
 	@Override
-	public void setUp() throws Exception{
+	public void setUp() throws Exception {
 		super.setUp();
 		context = getContext();
 		db = new DatabaseHandler(context);
@@ -27,42 +26,57 @@ public class DatabaseHandlerTest extends AndroidTestCase{
 	@Test
 	public void testAddWeather() {
 		db.addWeather(weather1);
-		db.addWeather(new Weather("12", 65.3,86.2,100,87,50));
-		assertNotNull(db.getWeather("57.2","52.3"));
+		db.addWeather(new Weather("12", 65.3, 86.2, 100, 87, 50));
+		assertNotNull(db.getWeather("65.3", "86.2"));
+		assertNotNull(db.getWeather("57.2", "52.3"));
 	}
-	
+
 	@Test
-	public void testGetWeather(){
-		Weather testWeather = db.getWeather("65.3","86.2");
-		assertEquals(50,testWeather.getChanceOfPrecipi());
-		assertEquals("12",testWeather.getTime());
-		assertEquals(65.3,testWeather.getLatitude());
-		assertEquals(86.2,testWeather.getLongitude());
-		assertEquals(100,testWeather.getMaxTemp());
-		assertEquals(87,testWeather.getMinTemp());
+	public void testGetWeather() {
+		db.addWeather(new Weather("12", 65.3, 86.2, 100, 87, 50));
+		Weather testWeather = db.getWeather("65.3", "86.2");
+		assertEquals(50, testWeather.getChanceOfPrecipi());
+		assertEquals("12", testWeather.getTime());
+		assertEquals(65.3, testWeather.getLatitude());
+		assertEquals(86.2, testWeather.getLongitude());
+		assertEquals(100, testWeather.getMaxTemp());
+		assertEquals(87, testWeather.getMinTemp());
 	}
-	
+
 	@Test
-	public void testDeleteWeather(){
-		Weather testWeather = db.getWeather("57.2","52.3");
+	public void testDeleteWeather() {
+		db.addWeather(weather1);
+		Weather testWeather = db.getWeather("57.2", "52.3");
 		db.deleteWeather(testWeather);
 	}
-	
+
 	@Test
-	public void testGetAllWeather(){
+	public void testGetAllWeather() {
+		db.addWeather(weather1);
+		db.addWeather(new Weather("12", 65.3, 86.2, 100, 87, 50));
 		List<Weather> allWeather = db.getAllWeather();
-		 for (Weather cn : allWeather) {
-	            String log = "Time: "+cn.getTime()+" ,Latitude: " + cn.getLatitude() + " ,Longitude: " + cn.getLongitude() + " ,MaxTemp: " + cn.getMaxTemp();
-	                // Writing Contacts to log
-	        Log.d("Name: ", log);
-		 }
+		Weather testWeather1 = allWeather.get(0);
+		Weather testWeather2 = allWeather.get(1);
+		assertEquals(50, testWeather2.getChanceOfPrecipi());
+		assertEquals("12", testWeather2.getTime());
+		assertEquals(65.3, testWeather2.getLatitude());
+		assertEquals(86.2, testWeather2.getLongitude());
+		assertEquals(100, testWeather2.getMaxTemp());
+		assertEquals(87, testWeather2.getMinTemp());
+		
+		assertEquals(50, testWeather1.getChanceOfPrecipi());
+		assertEquals("1", testWeather1.getTime());
+		assertEquals(57.2, testWeather1.getLatitude());
+		assertEquals(52.3, testWeather1.getLongitude());
+		assertEquals(75, testWeather1.getMaxTemp());
+		assertEquals(50, testWeather1.getMinTemp());
 	}
-	
+
 	@Override
-	public void tearDown() throws Exception{
+	public void tearDown() throws Exception {
+		context.deleteDatabase("weatherManager");
 		db.close();
-        context.deleteDatabase(""+db.getWritableDatabase());
-        super.tearDown();
-    }
+		super.tearDown();
+	}
 
 }
