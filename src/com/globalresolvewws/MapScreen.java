e739dd11836cf3f5573c1500aaf6386a5d60ae90
Brian Lau <ir00ber@gmail.com>
2013-03-15 12:35:41 -0700

    Completed Bluetooth in baseScreen, much of it was taken from sample Bluetooth Chat Application from SDK, just minus some methods

--- src/com/globalresolvewws/MapScreen.java
+++ src/com/globalresolvewws/MapScreen.java
@@ -2,15 +2,27 @@ package com.globalresolvewws;
 
 //import android.app.Activity;
 import com.globalresolvewws.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
 
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 
 public class MapScreen extends FragmentActivity {
-
+	private GoogleMap myMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_screen);
+        
+        FragmentManager myFragmentManager = getSupportFragmentManager();
+        SupportMapFragment mySupportMapFragment 
+         = (SupportMapFragment)myFragmentManager.findFragmentById(R.id.map);
+        myMap = mySupportMapFragment.getMap();
+        
+        myMap.setMyLocationEnabled(true);
+        
+        myMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
     }
 }
