3d61fec01fc191028b1a019c7a399e507cac2c5a
calebrogers <caleb.w.rogers@gmail.com>
2013-03-16 22:29:50 -0700

    Updated servicehandler to populate a list of weather objects

--- src/com/globalresolvewws/ServiceHandler.java
+++ src/com/globalresolvewws/ServiceHandler.java
@@ -1,15 +1,21 @@
 package com.globalresolvewws;
 
+/**
+ * This handler populates a list of weather objects off of the service
+ * it is based off of work found on stackoverflow
+ * @author Caleb, (?)
+ * 
+ */
+
+
 import java.util.ArrayList;
 import org.xml.sax.Attributes;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class ServiceHandler extends DefaultHandler {
 
-	// Create three array lists to store the data
-	public ArrayList<Integer> tempLows = new ArrayList<Integer>();
-	public ArrayList<Integer> tempHighs = new ArrayList<Integer>();
-	public ArrayList<String> days = new ArrayList<String>();
+	// Create an arraylist with the weather information
+	public ArrayList<Weather> forecast = new ArrayList<Weather>();
 
 	// Make sure that the code in DefaultHandler's
 	//   constructor is called:
@@ -29,20 +35,27 @@ public class ServiceHandler extends DefaultHandler {
 
 	// This is where all the work is happening:
 	public void startElement (String uri, String name, String qName, Attributes atts) {
+		Weather entry = new Weather();
 		if (qName.compareTo("day_of_week") == 0) {
 			String day = atts.getValue(0);
-			System.out.println("Day: " + day);
-			days.add(day);
+			entry.setTime(day);
 		}
 		if(qName.compareToIgnoreCase("low") == 0) {
 			int low = Integer.parseInt(atts.getValue(0));
-			System.out.println("Low: " + low);
-			tempLows.add(low);
+			entry.setMinTemp(low);
 		}
 		if(qName.compareToIgnoreCase("high") == 0) {
 			int high = Integer.parseInt(atts.getValue(0));
-			System.out.println("High: " + high);
-			tempHighs.add(high);
+			entry.setMaxTemp(high);
 		}
+		//if(qName.compareToIgnoreCase("condition") == 0) {
+		//	String condition = atts.getValue(0);
+		//	entry.set(condition);
+		//}
+		if(qName.compareToIgnoreCase("humidity") == 0) {
+			int humid = Integer.parseInt(atts.getValue(0));
+			entry.setChanceOfPrecipi(humid);
+		}
+		forecast.add(entry);
 	}
 }
