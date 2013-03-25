171b24bb869027c35c188e7afb9078db0ccd28e6
Brian Lau <ir00ber@gmail.com>
2013-03-25 12:57:17 -0700

    Began testing BaseClass again, using powermock for bluetooth portions

--- src/com/globalresolvewws/test/BaseScreenTest.java
+++ src/com/globalresolvewws/test/BaseScreenTest.java
@@ -1,21 +1,33 @@
 package com.globalresolvewws.test;
 
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.powermock.api.easymock.PowerMock.mockStatic;
+import static org.powermock.api.easymock.PowerMock.replayAll;
+import static org.powermock.api.easymock.PowerMock.verifyAll;
+import static org.easymock.EasyMock.expect;
+
 import com.globalresolvewws.R;
 import com.globalresolvewws.R.id;
 import com.globalresolvewws.BaseScreen;
 
+import android.bluetooth.BluetoothDevice;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({BluetoothDevice.class})
 public class BaseScreenTest extends
 		ActivityInstrumentationTestCase2<BaseScreen> {
 
 	private BaseScreen mActivity;
 	private ImageView mImageView;
 	private TextView mTextView;
-	public static final boolean INITIAL_POSITION = false;
+	private Button mMapViewButton;
 
 	public BaseScreenTest() {
 		super("com.globalresovlewws", BaseScreen.class);
@@ -31,11 +43,13 @@ public class BaseScreenTest extends
 				.findViewById(com.globalresolvewws.R.id.imageView);
 		mTextView = (TextView) mActivity
 				.findViewById(com.globalresolvewws.R.id.temp_curr);
+		mMapViewButton = (Button) mActivity.findViewById(com.globalresolvewws.R.id.map);
 	}
 
 	public void testPresetUp() {
 		assertTrue(mImageView != null);
 		assertTrue(mTextView != null);
+		assertTrue(mMapViewButton != null);
 	}
 
 	public void testMainActivityUI() {
